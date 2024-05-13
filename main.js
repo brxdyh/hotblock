cf15ed3a7927fa2eb94d062e996c3dfc7abdd1a4
brxdyh <hatchbrady311@gmail.com>
2024-05-13 19:48:45 -0400

    slight additions to the js script, added another leaflet library file

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+const lat = 38.91473296688135;
+const lon = -75.42635549832752;
+
 // Function to load GeoJSON data from a file and add it to the map
 // I used AI for this lol so there is probably a better way
+var filePath = "map.geojson";
+
 function addGeoJSONLayerToMap(filePath) {
   fetch(filePath)
     .then((response) => response.json())
@@ -22,13 +27,22 @@ function addGeoJSONLayerToMap(filePath) {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var map = L.map("map").setView([38.91473296688135, -75.42635549832752], 17); // Sets initial coordinates (Arena's) and zoom level
+  var map = L.map("map", {
+    renderer: L.canvas({ tolerance: 10 }),
+  }).setView([lat, lon], 17); // Sets initial coordinates (Arena's) and zoom level
+
+  map.setMaxBounds([
+    [73.5107240340998, -175.02086457138665],
+    [-48.995311187950925, 175.02086457138665],
+  ]);
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     maxZoom: 19,
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    zoomControl: false,
+    minZoom: 3,
+    noWrap: true,
   }).addTo(map);
-  var filePath = "map.geojson";
   addGeoJSONLayerToMap(filePath);
 });
